30e4dc3b329bbfd66164e9b9cddb6617e4c39620
Abhijeet Mankani <abhijeet.mankani@tavant.com>
2026-08-18 11:45:44 +0530

    Initializing Backend

---
+++ Backend/LoanLite/src/main/java/com/loanlite/loanlite/LoanLiteApplication.java
@@ -0,0 +1,12 @@
+package com.loanlite.loanlite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanLiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanLiteApplication.class, args);
+	}
+}
---
+++ Backend/LoanLite/src/test/java/com/loanlite/loanlite/LoanLiteApplicationTests.java
@@ -0,0 +1,11 @@
+package com.loanlite.loanlite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanLiteApplicationTests {
+
+	@Test
+	void contextLoads() {}
+}
